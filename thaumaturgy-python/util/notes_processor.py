6ce0004b1f50de6a676708a8022cf4d0e484a773
Nicole Venner <nvenner@protonmail.ch>
2024-09-17 18:41:31 -0600

    more refactoring, should be done for now

--- thaumaturgy-python/util/notes_processor.py
+++ thaumaturgy-python/util/notes_processor.py
@@ -1,5 +1,4 @@
-from common.llm_utils import ChatRole, KeChatMessage
-from rag.rag_engine import KeRagEngine
+from common.llm_utils import ChatRole, KeChatMessage, KeLLMUtils
 from util.gpu_compute_calls import audio_to_text_api
 from pydantic import BaseModel
 from pathlib import Path
@@ -16,27 +15,10 @@ class TranscribedNote(BaseModel):
     summary: Optional[str] = None
 
 
-llm = KeRagEngine("llama70b")
+llm = KeLLMUtils("llama70b")
 
 
 # Moving this to another file might be a good idea. Plus throwing the semantic splitter to split on sentnaces is probably a good idea.
-async def mapreduce_llm_instruction_across_string(
-    content: str, chunk_size: int, instruction: str, join_str: str
-) -> str:
-    split = token_split(content, chunk_size)
-
-    async def clean_chunk(chunk: str) -> str:
-
-        history = [
-            KeChatMessage(content=instruction, role=ChatRole.assistant),
-            KeChatMessage(content=chunk, role=ChatRole.user),
-        ]
-        completion = await llm.achat(history)
-        return completion.content
-
-    tasks = [clean_chunk(chunk) for chunk in split]
-    results = await asyncio.gather(*tasks)
-    return join_str.join(results)
 
 
 async def process_note_task(
@@ -50,14 +32,14 @@ async def process_note_task(
     raw_transcript = await audio_to_text_api(filepath, lang)
     clean_chunk_prompt = "You are a transcription cleanup robot. Please take in the chunk of transcript provided by the user, and return a version of the transcript that doesnt have any repeated phrases, umms, ahhs or sentance fragments. DO NOT SUMMARIZE THE TRANSCRIPT. Try to match every word verbatim except when cleaning the sentances. Don't say anything to the user other then providing the cleaned transcript."
 
-    clean_transcript = await mapreduce_llm_instruction_across_string(
+    clean_transcript = await llm.mapreduce_llm_instruction_across_string(
         content=raw_transcript,
         chunk_size=NOTE_CHUNK_SIZE,
         instruction=clean_chunk_prompt,
         join_str="",
     )
     create_notes_prompt = "You are a note taking assistant. Please take in the transcript provided by the user and return detailed notes that outlines the key points of the discussion. Make sure to take note of anything the group came to concensus on, any items on the agenda for the meeting and any action items that the group decided to take."
-    notes = await mapreduce_llm_instruction_across_string(
+    notes = await llm.mapreduce_llm_instruction_across_string(
         content=clean_transcript,
         chunk_size=TRANSCRIPT_CHUNK_SIZE,
         instruction=create_notes_prompt,
