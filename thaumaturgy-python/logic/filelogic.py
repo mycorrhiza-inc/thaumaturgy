9e2b566f34c786664ff773cdfe09cbf58430c1bc
Nicole Venner <nvenner@protonmail.ch>
2024-10-23 09:45:18 -0600

    fixed bug

--- thaumaturgy-python/logic/filelogic.py
+++ thaumaturgy-python/logic/filelogic.py
@@ -111,7 +111,7 @@ async def upsert_full_file_to_db(
     else:
         id = str(obj.id)
         url = f"https://kessler.xyz/api/v2/public/files/{id}"
-    json_data = obj.model_dump()
+    json_data = json.loads(obj.model_dump_json())
     for _ in range(5):
         async with aiohttp.ClientSession() as session:
             async with session.post(url, json=json_data) as response:
